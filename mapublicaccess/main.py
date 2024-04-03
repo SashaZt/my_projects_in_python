03a2b530231c6a910db596d1be010e31671b23f1
zinchyk.a <a.zinchyk83@gmail.com>
2024-04-03 11:04:51 +0300

    03042024

--- mapublicaccess/main.py
+++ mapublicaccess/main.py
@@ -5,7 +5,7 @@ from playwright.async_api import async_playwright
 import aiohttp
 import aiofiles
 import re
-import string
+import sys
 import csv
 import re
 import os
@@ -13,37 +13,37 @@ import glob
 from asyncio import sleep
 
 
-async def download_file(session, url, cookies_dict, filename_pdf):
-    headers = {
-        "authority": "www.assessedvalues2.com",
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "accept-language": "ru,en-US;q=0.9,en;q=0.8,uk;q=0.7,de;q=0.6",
-        "cache-control": "no-cache",
-        # 'cookie': 'ASP.NET_SessionId=w1lubbprygi3wq5hdfiwa0tl; CookieTest=Testme; sucuri_cloudproxy_uuid_0766875d6=399c6876557455524af4b491910baaac; SearchList2=; SearchList3=; SearchList=000101',
-        "dnt": "1",
-        "pragma": "no-cache",
-        "referer": "https://www.assessedvalues2.com/SearchPage.aspx?jurcode=112",
-        "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "same-origin",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
-    }
+# async def download_file(session, url, cookies_dict, filename_pdf):
+#     headers = {
+#         "authority": "www.assessedvalues2.com",
+#         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+#         "accept-language": "ru,en-US;q=0.9,en;q=0.8,uk;q=0.7,de;q=0.6",
+#         "cache-control": "no-cache",
+#         # 'cookie': 'ASP.NET_SessionId=w1lubbprygi3wq5hdfiwa0tl; CookieTest=Testme; sucuri_cloudproxy_uuid_0766875d6=399c6876557455524af4b491910baaac; SearchList2=; SearchList3=; SearchList=000101',
+#         "dnt": "1",
+#         "pragma": "no-cache",
+#         "referer": "https://www.assessedvalues2.com/SearchPage.aspx?jurcode=112",
+#         "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+#         "sec-ch-ua-mobile": "?0",
+#         "sec-ch-ua-platform": '"Windows"',
+#         "sec-fetch-dest": "document",
+#         "sec-fetch-mode": "navigate",
+#         "sec-fetch-site": "same-origin",
+#         "sec-fetch-user": "?1",
+#         "upgrade-insecure-requests": "1",
+#         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+#     }
 
-    async with session.get(url, headers=headers, cookies=cookies_dict) as response:
-        if response.status == 200:
-            async with aiofiles.open(filename_pdf, "wb") as out_file:
-                while True:
-                    chunk = await response.content.read(1024)
-                    if not chunk:
-                        break
-                    await out_file.write(chunk)
-        else:
-            print(f"Ошибка при загрузке файла: {response.status}")
+#     async with session.get(url, headers=headers, cookies=cookies_dict) as response:
+#         if response.status == 200:
+#             async with aiofiles.open(filename_pdf, "wb") as out_file:
+#                 while True:
+#                     chunk = await response.content.read(1024)
+#                     if not chunk:
+#                         break
+#                     await out_file.write(chunk)
+#         else:
+#             print(f"Ошибка при загрузке файла: {response.status}")
 async def create_directories_async(folders_list):
     for folder in folders_list:
         if not os.path.exists(folder):
@@ -59,6 +59,14 @@ async def read_csv_values():
             values.append(line.strip())
     return values
 
+log_file_path = 'log.txt'
+
+if os.path.exists(log_file_path):
+    # Если файл существует, удаляем его
+    os.remove(log_file_path)
+async def write_log(message):
+    async with aiofiles.open('log.txt', 'a', encoding="utf-8") as log_file:
+        await log_file.write(message + '\n')
 
 async def run():
     # Создайте полный путь к папке temp
@@ -76,15 +84,6 @@ async def run():
     )
     url_start = 'https://mapublicaccess.tylerhost.net/search/CommonSearch.aspx?mode=PARID'
     """По буквам поиск"""
-    # print("Введите начальную букву улицы")
-    # inpStreet = str(input())
-    # inpStreet = 'A'
-    # print("Введите юрисдикцию, MA199 - как пример")
-    # inpCity = str(input())
-    # inpCity = 'MA199'
-    
-    # print("Введите диапозон поиска по кодам, до")
-    # range_b = int(input())
     current_directory = os.getcwd()
     # Создайте полный путь к папке temp
     temp_path = os.path.join(current_directory, "temp")
@@ -114,7 +113,7 @@ async def run():
 
         parcel_set = set()
 
-        for v in values[:1]:
+        for v in values:
             xpath_inpNumber = '//input[@id="inpParid"]'
             await page.wait_for_selector(f"xpath={xpath_inpNumber}", timeout=timeout)
             await page.fill(xpath_inpNumber, str(f"{v}*"))
@@ -133,7 +132,6 @@ async def run():
                 match = re.search(r"Total found: (\d+) records", element_text)
                 if match:
                     total_records = int(match.group(1))
-                    print(f"Total records found: {total_records}")
                     if total_records >= 250:
                         # Обрабатываем ситуацию, когда найдено более 250 записей
                         sort_by_parcel_id = False
@@ -158,10 +156,6 @@ async def run():
 
                                 # Получаем содержимое новой страницы
                                 td_elements = await new_page.query_selector_all('td.NewLink')
-
-                                # Инициализируем пустой список для хранения извлеченных текстовых значений
-                                td_texts = []
-
                                 # Проходим по каждому найденному элементу
                                 for td_element in td_elements:
                                     # Получаем текстовое содержимое элемента и добавляем его в список
@@ -171,7 +165,7 @@ async def run():
                                 # По завершении работы с новой страницей не забудьте ее закрыть
                                 await new_page.close()
                             else:
-                                print('НЕТ xpath_printSearch')
+                                await write_log('НЕТ xpath_printSearch')
 
                             
                                 
@@ -191,10 +185,6 @@ async def run():
                     # Получаем содержимое новой страницы
                     td_elements = await new_page.query_selector_all('td.NewLink')
 
-                    # Инициализируем пустой список для хранения извлеченных текстовых значений
-                    td_texts = []
-
-                    # Проходим по каждому найденному элементу
                     for td_element in td_elements:
                         # Получаем текстовое содержимое элемента и добавляем его в список
                         text = await td_element.text_content()
@@ -203,10 +193,25 @@ async def run():
                     # По завершении работы с новой страницей не забудьте ее закрыть
                     await new_page.close()
                 else:
-                    print(f"Одна запись {v}")
+                    await write_log(f"Одна запись {v}")
+                    await page.goto(url_start)
+                    
 
 
-            print(len(parcel_set))
+        filename = 'parcels.csv'
+
+        # Открываем файл на запись
+        with open(filename, 'w', newline='', encoding='utf-8') as file:
+            # Создаем объект writer
+            writer = csv.writer(file)
+            
+            # Проходим по каждому значению из набора
+            for value in parcel_set:
+                # Записываем значение в новую строку файла
+                writer.writerow([value])
+        print("Все скачано")
+        await sleep(5)
+        await browser.close()
             # # Ожидаем, что элемент с информацией о количестве найденных записей станет доступен на странице.
             # try:
             #     await page.wait_for_selector('text=Total found')
@@ -353,82 +358,82 @@ async def run():
         #     # Запись каждого уникального текстового значения из set в новую строку файла
         #     for parcel_number in parcel_set:
         #         writer.writerow([parcel_number])
-        exit()
+        
 
-        match = re.search(r"jurcode=(\d+)", url_start)
+        # match = re.search(r"jurcode=(\d+)", url_start)
 
-        jurcode = match.group(1)
-        # Ждем появление кнопки поиска и нажимаем на нее
-        xpath_begin_search = '//input[@id="ctl00_MainContent_BtnSearch"]'
-        # Дожидаемся появления кнопки с заданным текстом и кликаем по ней
-        await page.wait_for_selector(f"xpath={xpath_begin_search}", timeout=timeout)
-        await page.click(xpath_begin_search)
-        await asyncio.sleep(1)
-        # Ждем появление поля ввода, вводим значение из переменной current и нажимаем Enter
-        xpath_keyno = '//input[@id="ctl00_MainContent_TxtKey"]'
-        await page.wait_for_selector(f"xpath={xpath_keyno}", timeout=timeout)
-        folder_pdf = os.path.join(pdf_path, "*.pdf")
-        files_pdf = glob.glob(folder_pdf)
-        found_parts = []
+        # jurcode = match.group(1)
+        # # Ждем появление кнопки поиска и нажимаем на нее
+        # xpath_begin_search = '//input[@id="ctl00_MainContent_BtnSearch"]'
+        # # Дожидаемся появления кнопки с заданным текстом и кликаем по ней
+        # await page.wait_for_selector(f"xpath={xpath_begin_search}", timeout=timeout)
+        # await page.click(xpath_begin_search)
+        # await asyncio.sleep(1)
+        # # Ждем появление поля ввода, вводим значение из переменной current и нажимаем Enter
+        # xpath_keyno = '//input[@id="ctl00_MainContent_TxtKey"]'
+        # await page.wait_for_selector(f"xpath={xpath_keyno}", timeout=timeout)
+        # folder_pdf = os.path.join(pdf_path, "*.pdf")
+        # files_pdf = glob.glob(folder_pdf)
+        # found_parts = []
 
-        # Обход всех файлов и сбор номеров в заданном диапазоне
-        for item in files_pdf:
-            filename = os.path.basename(item)
-            parts = filename.split("_")
-            if len(parts) >= 2:
-                try:
-                    part_number = int(parts[1])  # Извлекаем номер
-                    if range_a <= part_number <= range_b:
-                        found_parts.append(part_number)
-                except ValueError:
-                    # Если part2 не является числом, пропускаем этот файл
-                    continue
+        # # Обход всех файлов и сбор номеров в заданном диапазоне
+        # for item in files_pdf:
+        #     filename = os.path.basename(item)
+        #     parts = filename.split("_")
+        #     if len(parts) >= 2:
+        #         try:
+        #             part_number = int(parts[1])  # Извлекаем номер
+        #             if range_a <= part_number <= range_b:
+        #                 found_parts.append(part_number)
+        #         except ValueError:
+        #             # Если part2 не является числом, пропускаем этот файл
+        #             continue
 
-        # Определяем отсутствующие номера в диапазоне
-        missing_parts = [n for n in range(range_a, range_b + 1) if n not in found_parts]
+        # # Определяем отсутствующие номера в диапазоне
+        # missing_parts = [n for n in range(range_a, range_b + 1) if n not in found_parts]
 
-        # Определяем номер, с которого начать обработку
-        # Если в missing_parts есть элементы, берем первый как начальный номер для обработки
-        current = missing_parts[0] if missing_parts else range_b + 1
+        # # Определяем номер, с которого начать обработку
+        # # Если в missing_parts есть элементы, берем первый как начальный номер для обработки
+        # current = missing_parts[0] if missing_parts else range_b + 1
 
-        while current <= range_b:
-            if current in found_parts:
-                current += 1
-                continue
+        # while current <= range_b:
+        #     if current in found_parts:
+        #         current += 1
+        #         continue
 
-            await page.fill(xpath_keyno, str(current))
-            await page.press(xpath_keyno, "Enter")
-            # Получаем куки из контекста браузера
-            cookies = await context.cookies()
-            cookies_dict = {cookie["name"]: cookie["value"] for cookie in cookies}
-            # Ждем появление ссылки и получаем с нее href
-            try:
-                xpath_href = '//a[@target="_blank"]'
-                await page.wait_for_selector(f"xpath={xpath_href}", timeout=timeout)
-                url_href = await page.get_attribute(xpath_href, "href")
+        #     await page.fill(xpath_keyno, str(current))
+        #     await page.press(xpath_keyno, "Enter")
+        #     # Получаем куки из контекста браузера
+        #     cookies = await context.cookies()
+        #     cookies_dict = {cookie["name"]: cookie["value"] for cookie in cookies}
+        #     # Ждем появление ссылки и получаем с нее href
+        #     try:
+        #         xpath_href = '//a[@target="_blank"]'
+        #         await page.wait_for_selector(f"xpath={xpath_href}", timeout=timeout)
+        #         url_href = await page.get_attribute(xpath_href, "href")
 
-                pattern = r"pdf=([^&]+)"
-                match = re.search(pattern, url_href)
+        #         pattern = r"pdf=([^&]+)"
+        #         match = re.search(pattern, url_href)
 
-                if match:
-                    extracted_part = match.group(1)
-                    keyno_match = re.search(r'K(\d+)N', extracted_part)
-                    keyno = keyno_match.group(1) if keyno_match else None
-                else:
-                    print("Совпадение не найдено.")
-                url = f"https://www.assessedvalues2.com/pdfs/{jurcode}/{extracted_part}.pdf"
-                filename_pdf = os.path.join(
-                    pdf_path, f"{jurcode}_{keyno}_{extracted_part}.pdf"
-                )
-                await download_file(session, url, cookies_dict, filename_pdf)
-                current += 1
-            except:
-                current += 1
-                continue
+        #         if match:
+        #             extracted_part = match.group(1)
+        #             keyno_match = re.search(r'K(\d+)N', extracted_part)
+        #             keyno = keyno_match.group(1) if keyno_match else None
+        #         else:
+        #             print("Совпадение не найдено.")
+        #         url = f"https://www.assessedvalues2.com/pdfs/{jurcode}/{extracted_part}.pdf"
+        #         filename_pdf = os.path.join(
+        #             pdf_path, f"{jurcode}_{keyno}_{extracted_part}.pdf"
+        #         )
+        #         await download_file(session, url, cookies_dict, filename_pdf)
+        #         current += 1
+        #     except:
+        #         current += 1
+        #         continue
                 
-        print("Все скачано")
-        await sleep(5)
-        await browser.close()
+        # print("Все скачано")
+        # await sleep(5)
+        # await browser.close()
     # elif collection_method == 1:
     #     lowercase_letters_list = list(string.ascii_lowercase)
     #     for letter_streed in lowercase_letters_list:
@@ -589,5 +594,19 @@ async def run():
     #             await browser.close()
 
 
+if __name__ == "__main__":
+    while True:
+        print('Введите 1 для запуска парсинга'
+            '\nВведите 0 для закрытия программы')
+        user_input = int(input("Выберите действие: "))
 
-asyncio.run(run())
+        if user_input == 1:
+            asyncio.run(run())
+        elif user_input == 0:
+            print("Программа завершена.")
+            sys.exit(1)
+        else:
+            print("Неверный ввод, пожалуйста, введите корректный номер действия.")
+        
+
+
