6ba9c9d4c1a0388b3320305375b79db77c20b736
zinchyk.a <a.zinchyk83@gmail.com>
2024-06-17 15:35:16 +0300

    17062024

--- 3dsky_org/main.py
+++ 3dsky_org/main.py
@@ -6,7 +6,6 @@ import os
 import random
 import shutil
 
-
 current_directory = os.getcwd()
 temp_path = os.path.join(current_directory, "temp")
 no_img_path = os.path.join(temp_path, "no_img")
@@ -22,24 +21,35 @@ def get_random_pause(time_pause):
 def rename_archive_if_needed(file_name, file_name_archive):
     if file_name != file_name_archive:
         archive_extensions = {".zip", ".rar", ".7z"}
-        for ext in archive_extensions:
-            archive_path = os.path.join(temp_path, f"{file_name}{ext}")
+        for archive_ext in archive_extensions:
+            archive_path = os.path.join(temp_path, f"{file_name}{archive_ext}")
             if os.path.exists(archive_path):
-                new_archive_path = os.path.join(temp_path, f"{file_name_archive}{ext}")
+                new_archive_path = os.path.join(
+                    temp_path, f"{file_name_archive}{archive_ext}"
+                )
                 os.rename(archive_path, new_archive_path)
                 print(f"Переименован архив {archive_path} -> {new_archive_path}")
                 break
 
 
+# Функция для принудительного перемещения архива
+def move_archive_forcefully(src_path, dst_path):
+    if os.path.exists(dst_path):
+        os.remove(dst_path)  # Удаляем файл, если он существует
+    shutil.move(src_path, dst_path)
+    print(f"Перемещен архив {src_path} -> {dst_path}")
+
+
 # Основная функция
 def main(time_pause):
     start_url = "https://b6.3ddd.ru/media/cache/sky_model_new_big_ang_en/"
+    archive_extensions = {".zip", ".rar", ".7z"}
 
     file_names = [
         os.path.splitext(f)[0]
         for f in os.listdir(temp_path)
         if os.path.isfile(os.path.join(temp_path, f))
-        and os.path.splitext(f)[1].lower() in {".zip", ".rar", ".7z"}
+        and os.path.splitext(f)[1].lower() in archive_extensions
     ]
 
     cookies = {
@@ -64,39 +74,116 @@ def main(time_pause):
     }
 
     for file_name in file_names:
+        # Проверка наличия файлов с изображениями
+        image_extensions = {".jpg", ".jpeg", ".png"}
+        image_exists = any(
+            os.path.exists(os.path.join(temp_path, f"{file_name}{ext}"))
+            for ext in image_extensions
+        )
+        if image_exists:
+            print(f"Изображение для {file_name} уже существует.")
+            continue  # Пропускаем файл, если изображение уже существует
 
+        # Формируем данные для запроса
         json_data = {
             "query": file_name,
             "order": "relevance",
         }
+
+        # Отправляем запрос на API для поиска модели
         response = requests.post(
             "https://3dsky.org/api/models",
             cookies=cookies,
             headers=headers,
             json=json_data,
         )
-        file_name_jpeg = f"{file_name}.jpeg"
-        jpeg_path = os.path.join(temp_path, file_name_jpeg)
-        if not os.path.exists(jpeg_path):
-            try:
-                json_data = response.json()
-                if (
-                    json_data["data"]["models"]
-                    and json_data["data"]["models"][0]["images"]
-                ):
-                    image_url_raw = json_data["data"]["models"][0]["images"][0][
-                        "web_path"
-                    ]
-                    image_url = f"{start_url}{image_url_raw}"
-                    response = requests.get(image_url)
+        if response.status_code != 200:
+            print(
+                f"Не удалось получить данные для {file_name}. Код ошибки: {response.status_code}"
+            )
+            continue  # Пропускаем файл, если запрос не успешен
 
-                    match = re.search(r"/([^/]+)\.jpeg$", image_url)
+        json_data = response.json()
+        if not (json_data.get("data") and json_data["data"].get("models")):
+            print(f"Не удалось найти изображение для {file_name}. Перемещение архива.")
+            for ext in archive_extensions:
+                archive_path = os.path.join(temp_path, f"{file_name}{ext}")
+                if os.path.exists(archive_path):
+                    dst_path = os.path.join(no_img_path, f"{file_name}{ext}")
+                    move_archive_forcefully(archive_path, dst_path)
+                    break
+            continue  # Пропускаем файл, если данные модели не найдены
+
+        found_image = False
+
+        # Проходим по всем моделям в данных
+        for model in json_data["data"]["models"]:
+            if not model.get("images"):
+                continue  # Пропускаем модель, если у нее нет изображений
+
+            for i, image_data in enumerate(model["images"]):
+                if image_data["file_name"] == file_name:
+                    found_image = True
+
+                    if i == 0:  # Если найденное изображение первое, скачиваем его
+                        image_url_raw = image_data["web_path"]
+                        image_url = f"{start_url}{image_url_raw}"
+                        response = requests.get(image_url)
+
+                        match = re.search(
+                            r"/([^/]+)\.(jpg|jpeg|png)$", image_url, re.IGNORECASE
+                        )
+                        if match:
+                            file_name_archive = match.group(1)
+                            ext = match.group(2)
+                            image_name = f"{file_name_archive}.{ext}"
+                            image_path = os.path.join(temp_path, image_name)
+
+                            if response.status_code == 200:
+                                with open(image_path, "wb") as f:
+                                    f.write(response.content)
+                                print(f"Изображение сохранено в {image_path}")
+                            else:
+                                print(
+                                    f"Не удалось скачать изображение. Код ошибки: {response.status_code}"
+                                )
+
+                            # Проверка и переименование архива после скачивания изображения
+                            rename_archive_if_needed(file_name, file_name_archive)
+
+                        break
+                    elif i > 0:
+                        # Проверяем наличие архива с таким же именем и удаляем его
+                        for archive_ext in archive_extensions:
+                            archive_path = os.path.join(
+                                temp_path, f"{file_name}{archive_ext}"
+                            )
+                            if os.path.exists(archive_path):
+                                os.remove(archive_path)
+                                print(f"Удален архив {archive_path}")
+                        break
+            if found_image:
+                break
+
+        if not found_image:
+            # Если запрашиваемое имя файла не найдено, берем первое изображение из списка
+            for model in json_data["data"]["models"]:
+                if not model.get("images"):
+                    continue  # Пропускаем модель, если у нее нет изображений
+
+                first_image_data = model["images"][0]
+                image_url_raw = first_image_data["web_path"]
+                image_url = f"{start_url}{image_url_raw}"
+                response = requests.get(image_url)
+
+                match = re.search(
+                    r"/([^/]+)\.(jpg|jpeg|png)$", image_url, re.IGNORECASE
+                )
+                if match:
                     file_name_archive = match.group(1)
-                    image_name = f"{file_name_archive}.jpeg"
+                    ext = match.group(2)
+                    image_name = f"{file_name_archive}.{ext}"
                     image_path = os.path.join(temp_path, image_name)
-
-                    # Проверка и переименование архива
-                    rename_archive_if_needed(file_name, file_name_archive)
 
                     if response.status_code == 200:
                         with open(image_path, "wb") as f:
@@ -106,21 +193,26 @@ def main(time_pause):
                         print(
                             f"Не удалось скачать изображение. Код ошибки: {response.status_code}"
                         )
-                else:
-                    raise IndexError("No images found")
-            except (KeyError, IndexError):
-                print(
-                    f"Не удалось найти изображение для {file_name}. Перемещение архива."
-                )
-                archive_extensions = {".zip", ".rar", ".7zip"}
-                for ext in archive_extensions:
-                    archive_path = os.path.join(temp_path, f"{file_name}{ext}")
-                    if os.path.exists(archive_path):
-                        shutil.move(archive_path, no_img_path)
-                        print(f"Перемещен архив {archive_path} -> {no_img_path}")
-                        break
-            random_pause = get_random_pause(time_pause)
-            time.sleep(random_pause)
+
+                    # Проверка и переименование архива после скачивания изображения
+                    rename_archive_if_needed(file_name, file_name_archive)
+
+                    found_image = True
+                    break
+
+        # Если изображение так и не было найдено и скачано, перемещаем архив
+        if not found_image:
+            print(f"Не удалось найти изображение для {file_name}. Перемещение архива.")
+            for ext in archive_extensions:
+                archive_path = os.path.join(temp_path, f"{file_name}{ext}")
+                if os.path.exists(archive_path):
+                    shutil.move(archive_path, no_img_path)
+                    print(f"Перемещен архив {archive_path} -> {no_img_path}")
+                    break
+
+        # Пауза перед обработкой следующего файла
+        random_pause = get_random_pause(time_pause)
+        time.sleep(random_pause)
 
 
 if __name__ == "__main__":
---
+++ repuve_gob/main_selenium_new.py
@@ -0,0 +1,306 @@
+#Запускаем через proxychains4 python3 main_selenium_new.py NUE2691
+import time
+import asyncio
+import requests
+import selenium
+import os
+import aiofiles
+import json
+import socks
+import socket
+import argparse
+import sys
+import pandas as pd
+import random
+import zipfile
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+from seleniumwire import webdriver
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.action_chains import ActionChains
+
+current_directory = os.getcwd()
+temp_path = os.path.join(current_directory, "temp")
+repuve_path = os.path.join(temp_path, "repuve")
+pgj_path = os.path.join(temp_path, "pgj")
+ocra_path = os.path.join(temp_path, "ocra")
+carfax_path = os.path.join(temp_path, "carfax")
+aviso_path = os.path.join(temp_path, "aviso")
+
+
+# Создание директории, если она не существует
+os.makedirs(temp_path, exist_ok=True)
+os.makedirs(repuve_path, exist_ok=True)
+os.makedirs(pgj_path, exist_ok=True)
+os.makedirs(ocra_path, exist_ok=True)
+os.makedirs(carfax_path, exist_ok=True)
+os.makedirs(aviso_path, exist_ok=True)
+
+
+def save_response_json(json_response, number, path):
+    """Синхронно сохраняет JSON-данные в файл."""
+    filename = os.path.join(path, f"{number}.json")
+    with open(filename, mode="w", encoding="utf-8") as f:
+        json.dump(json_response, f, ensure_ascii=False, indent=4)
+    print(filename)
+
+
+def load_config():
+    if getattr(sys, "frozen", False):
+        # Если приложение 'заморожено' с помощью PyInstaller
+        application_path = os.path.dirname(sys.executable)
+    else:
+        # Обычный режим выполнения (например, во время разработки)
+        application_path = os.path.dirname(os.path.abspath(__file__))
+
+    filename_config = os.path.join(application_path, "config.json")
+    if not os.path.exists(filename_config):
+        print("Нету файла с прокси-серверами!!!!!!!!!!!!!!!!!!!!!!!!!")
+        sys.exit(1)  # Завершаем выполнение скрипта с кодом ошибки 1
+    else:
+        with open(filename_config, "r") as config_file:
+            config = json.load(config_file)
+
+        return config
+
+
+def main(number):
+    time_wait = 30
+    config = load_config()
+    should_run_locally = (
+        False  # Переменная для выбора режима запуска (локально или удаленно)
+    )
+
+    if should_run_locally:
+        # Опции для локального запуска Chrome
+        options = Options()
+        options.add_experimental_option(
+            "detach", True
+        )  # Оставлять браузер открытым после выполнения скрипта
+        options.page_load_strategy = "eager"  # Стратегия загрузки страницы (быстрая)
+
+        # Инициализация драйвера для локального запуска
+        driver = webdriver.Chrome(
+            service=ChromeService(
+                ChromeDriverManager().install()
+            ),  # Установка ChromeDriver
+            options=options,
+        )
+    else:
+        # Настройка Selenium Wire с прокси SOCKS5
+        sw_options = {"addr": "127.0.0.1", "auto_config": False, "port": 8091}
+
+        # Опции для Chrome
+        chrome_options = Options()
+        # Установка стратегии загрузки страницы
+        chrome_options.page_load_strategy = "eager"
+
+        chrome_options.add_argument("--proxy-server=host.docker.internal:8091")
+        plugin_file = "proxy_auth_plugin.zip"
+
+        chrome_options.add_argument("--ignore-certificate-errors")
+
+        """Отключение загрузки изображений и других типов контента"""
+        prefs = {
+            "profile.managed_default_content_settings.images": 2,  # Отключение загрузки изображений
+            "profile.managed_default_content_settings.stylesheets": 2,  # Отключение загрузки стилей (CSS)
+            "profile.managed_default_content_settings.cookies": 2,  # Отключение сохранения и использования cookies
+            # "profile.managed_default_content_settings.javascript": 2,  # Отключение выполнения JavaScript
+            "profile.managed_default_content_settings.plugins": 2,  # Отключение загрузки плагинов
+            "profile.managed_default_content_settings.popups": 2,  # Отключение всплывающих окон
+            # "profile.managed_default_content_settings.geolocation": 2,  # Отключение доступа к геолокации
+            "profile.managed_default_content_settings.media_stream": 2,  # Отключение доступа к медиа-стримам (видео/аудио)
+        }
+
+        # chrome_options.add_experimental_option("prefs", prefs)
+
+        # Инициализация драйвера для удаленного запуска
+        driver = webdriver.Remote(
+            command_executor="http://127.0.0.1:4444/wd/hub",  # Адрес удаленного сервера Selenium
+            options=chrome_options,
+            seleniumwire_options=sw_options,  # Опции Selenium Wire
+        )
+
+    # Запускаем удаллено Selenium
+    try:
+        driver.implicitly_wait(time_wait)  # Установка неявного ожидания на 10 секунд
+        # driver.set_page_load_timeout(30)  # Установка таймаута для загрузки страницы
+        driver.maximize_window()  # Максимизация окна браузера
+
+        # Переход на заданный URL
+        driver.get("https://www2.repuve.gob.mx:8443/ciudadania/")
+        time.sleep(1)
+        # Первая кнопка при загрузке сайта
+        locator_entendido = (
+            By.CSS_SELECTOR,
+            ".swal2-styled.swal2-default-outline",
+        )
+        first_element_found = False
+        while not first_element_found:
+            try:
+                # Ждем, пока элемент станет доступным
+                element_entendido = WebDriverWait(driver, time_wait).until(
+                    ec.presence_of_element_located(locator_entendido)
+                )
+                first_element_found = True
+            except selenium.common.exceptions.TimeoutException:
+                driver.get("https://www2.repuve.gob.mx:8443/ciudadania/")
+                element_entendido = WebDriverWait(driver, time_wait).until(
+                    ec.presence_of_element_located(locator_entendido)
+                )
+
+        # Нажимаем на элемент
+        element_entendido.click()
+        # Поле для ввода номера
+        locator_numero_de_placa = (
+            By.CSS_SELECTOR,
+            "div:nth-child(3) > div:nth-child(1) > input",
+        )
+
+        input_numero_de_placa = WebDriverWait(driver, time_wait).until(
+            ec.presence_of_element_located(locator_numero_de_placa)
+        )
+        input_numero_de_placa.send_keys(number)
+
+        # Ищем кнопу для нажатия
+        locator_buscar = (
+            By.CSS_SELECTOR,
+            "div.clearfix > form > div > button.btn.btn-primary",
+        )
+        # Ждем, пока элемент станет доступным
+        button_buscar = WebDriverWait(driver, time_wait).until(
+            ec.presence_of_element_located(locator_buscar)
+        )
+
+        # Делаем клик на найденный элемент
+        button_buscar.click()
+        # Ждем загрузки следующей страницы
+        locator_next_element = (
+            By.CSS_SELECTOR,
+            "div:nth-child(1) > div > ul > li.active > a",
+        )
+
+        element_found = False
+        while not element_found:
+            try:
+                WebDriverWait(driver, time_wait).until(
+                    ec.presence_of_element_located(locator_next_element)
+                )
+                element_found = True
+            except selenium.common.exceptions.TimeoutException:
+                driver.get("https://www2.repuve.gob.mx:8443/ciudadania/")
+                locator_entendido = (
+                    By.CSS_SELECTOR,
+                    ".swal2-styled.swal2-default-outline",
+                )
+
+                # Ждем, пока элемент станет доступным
+                element_entendido = WebDriverWait(driver, time_wait).until(
+                    ec.presence_of_element_located(locator_entendido)
+                )
+
+                # Нажимаем на элемент
+                element_entendido.click()
+                # Определяем локатор для нового элемента
+                locator_numero_de_placa = (
+                    By.CSS_SELECTOR,
+                    "div:nth-child(3) > div:nth-child(1) > input",
+                )
+
+                # Ждем, пока элемент станет доступным
+                input_numero_de_placa = WebDriverWait(driver, time_wait).until(
+                    ec.presence_of_element_located(locator_numero_de_placa)
+                )
+                # Повторяем действия снова
+                input_numero_de_placa.send_keys(number)
+                locator_buscar = (
+                    By.CSS_SELECTOR,
+                    "div.clearfix > form > div > button.btn.btn-primary",
+                )
+                # Ждем, пока элемент станет доступным
+                button_buscar = WebDriverWait(driver, time_wait).until(
+                    ec.presence_of_element_located(locator_buscar)
+                )
+                button_buscar.click()
+        button_pgj_element = (
+            By.CSS_SELECTOR,
+            "div.col-sm-12 > div > div > div:nth-child(1) > div > ul > li:nth-child(2) > a",
+        )
+        button_pgj = WebDriverWait(driver, time_wait).until(
+            ec.presence_of_element_located(button_pgj_element)
+        )
+        button_pgj.click()
+        time.sleep(1)
+        button_ocra_element = (
+            By.CSS_SELECTOR,
+            "div.col-sm-12 > div > div > div:nth-child(1) > div > ul > li:nth-child(3) > a",
+        )
+        button_ocra = WebDriverWait(driver, time_wait).until(
+            ec.presence_of_element_located(button_ocra_element)
+        )
+        button_ocra.click()
+        time.sleep(1)
+        button_carfax_element = (
+            By.CSS_SELECTOR,
+            "div.col-sm-12 > div > div > div:nth-child(1) > div > ul > li:nth-child(4) > a",
+        )
+        button_carfax = WebDriverWait(driver, time_wait).until(
+            ec.presence_of_element_located(button_carfax_element)
+        )
+        button_carfax.click()
+        time.sleep(1)
+        button_aviso_element = (
+            By.CSS_SELECTOR,
+            "div.col-sm-12 > div > div > div:nth-child(1) > div > ul > li:nth-child(5) > a",
+        )
+        button_aviso = WebDriverWait(driver, time_wait).until(
+            ec.presence_of_element_located(button_aviso_element)
+        )
+        button_aviso.click()
+        time.sleep(1)
+        # Обработка запросов
+        url_to_path = {
+            "https://www2.repuve.gob.mx:8443/consulta/consulta/repuve": repuve_path,
+            "https://www2.repuve.gob.mx:8443/consulta/consulta/pgj": pgj_path,
+            "https://www2.repuve.gob.mx:8443/consulta/consulta/ocra": ocra_path,
+            "https://www2.repuve.gob.mx:8443/consulta/consulta/carfax": carfax_path,
+            "https://www2.repuve.gob.mx:8443/consulta/consulta/aviso": aviso_path,
+        }
+        for request in driver.requests:
+
+            if request.url in url_to_path:
+                try:
+                    if (
+                        request.response
+                        and "application/json"
+                        in request.response.headers.get("Content-Type", "")
+                    ):
+                        json_response = json.loads(
+                            request.response.body.decode("utf-8")
+                        )
+
+                        path = url_to_path[request.url]
+                        save_response_json(json_response, number, path)
+                except Exception as e:
+                    print(f"Error processing request {request.url}: {e}")
+
+    finally:
+        # Закрытие драйвера и завершение сеанса
+        driver.quit()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Script to run with a specific number parameter."
+    )
+    parser.add_argument("number", type=str, help="The number to be used in the script")
+    args = parser.parse_args()
+
+    main(args.number)
