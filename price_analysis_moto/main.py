da5c63877748c99832f4f6e015f5781f74efe993
zinchyk.a <a.zinchyk83@gmail.com>
2024-08-09 20:20:13 +0300

    09082024

--- price_analysis_moto/main.py
+++ price_analysis_moto/main.py
@@ -19,6 +19,7 @@ temp_path = os.path.join(current_directory, "temp")
 os.makedirs(temp_path, exist_ok=True)
 
 
+# Создание директорий согласно out.json
 def get_dir_json():
     # Загрузка данных из out.json
     with open("out.json", "r", encoding="utf-8") as file:
@@ -35,7 +36,7 @@ def get_dir_json():
             # Запись данных в новый JSON-файл
             json.dump({key: data[key]}, outfile, ensure_ascii=False, indent=4)
 
-    print("Данные успешно разделены и сохранены в соответствующих папках.")
+    logger.info("Данные успешно разделены и сохранены в соответствующих папках.")
     load_json_files_from_subdirectories()
 
 
@@ -67,9 +68,9 @@ def load_json_files_from_subdirectories():
                             all_links.append(l["link"])
                             id_link = l["id"]
                     except json.JSONDecodeError as e:
-                        print(f"Ошибка при загрузке {json_file_path}: {e}")
+                        logger.error(f"Ошибка при загрузке {json_file_path}: {e}")
             else:
-                print(f"Файл {json_file_path} не найден.")
+                logger.error(f"Файл {json_file_path} не найден.")
 
             # Запись ссылок в CSV
             with open(csv_file_path, "w", newline="", encoding="utf-8") as csv_file:
@@ -77,38 +78,11 @@ def load_json_files_from_subdirectories():
                 for link in all_links:
                     writer.writerow([link])
 
-    print("Все ссылки успешно записаны в CSV файлы.")
+    logger.info("Все ссылки успешно записаны в CSV файлы.")
 
 
 def create_html_and_read_csv():
-    cookies = {
-        "jrv": "55750",
-        "PHPSESSID": "5pfnuqtt1bh94bqaqs9qfv5ctu",
-        "default": "imrum01m616km4mgu38a4b0vpr",
-        "currency": "UAH",
-        "cf_clearance": "u7PFikBy.dJRMui6wRUUCmWA4Wbdu1nvD5FgD36KfvY-1723014419-1.0.1.1-QMhdwCrE.tZv_sGpWTNOXRP_x3OLH_jgX5.QlhvoSNGdK_JOqyi3V2oRHsp7_DbTqy11voEP.pI0u2DVX0V0Gg",
-        "language_url": "ru",
-        "language": "ru-ru",
-    }
 
-    headers = {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "accept-language": "ru,en-US;q=0.9,en;q=0.8,uk;q=0.7,de;q=0.6",
-        "cache-control": "no-cache",
-        # 'cookie': 'jrv=55750; PHPSESSID=5pfnuqtt1bh94bqaqs9qfv5ctu; default=imrum01m616km4mgu38a4b0vpr; currency=UAH; cf_clearance=u7PFikBy.dJRMui6wRUUCmWA4Wbdu1nvD5FgD36KfvY-1723014419-1.0.1.1-QMhdwCrE.tZv_sGpWTNOXRP_x3OLH_jgX5.QlhvoSNGdK_JOqyi3V2oRHsp7_DbTqy11voEP.pI0u2DVX0V0Gg; language_url=ru; language=ru-ru',
-        "dnt": "1",
-        "pragma": "no-cache",
-        "priority": "u=0, i",
-        "sec-ch-ua": '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "none",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
-    }
     dir_path = os.path.join(os.getcwd(), "temp")
 
     # Получение списка поддиректорий на верхнем уровне
@@ -116,42 +90,77 @@ def create_html_and_read_csv():
         d for d in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, d))
     ]
 
-    for directory in subdirs[2:3]:
-        # Полный путь к поддиректории
-        subdir_path = os.path.join(dir_path, directory)
+    for directory in subdirs:
+        if directory == "motodom_ua":
 
-        # Создание новой папки html в каждой поддиректории, если она еще не создана
-        html_path = os.path.join(subdir_path, "html")
-        if not os.path.exists(html_path):
-            os.makedirs(html_path)
+            # Полный путь к поддиректории
+            subdir_path = os.path.join(dir_path, directory)
+            # Создание новой папки html в каждой поддиректории, если она еще не создана
+            html_path = os.path.join(subdir_path, "html")
+            if not os.path.exists(html_path):
+                os.makedirs(html_path)
 
-        json_file_path = os.path.join(subdir_path, "out.json")
+            json_file_path = os.path.join(subdir_path, "out.json")
 
-        # Проверка на существование файла out.json
-        if os.path.exists(json_file_path):
-            with open(json_file_path, "r", encoding="utf-8") as json_file:
-                # Загрузка данных JSON
-                data = json.load(json_file)
-                # Получение всех ссылок из ключа "motodom_ua"
-                links = data.get("motodom_ua", [])
-                for l in links:
-                    url = l["link"]
-                    id_link = l["id"]
-                    file_name_html = os.path.join(html_path, f"{id_link}.html")
-                    if not os.path.exists(file_name_html):
-                        response = requests.get(
-                            url,
-                            cookies=cookies,
-                            headers=headers,
-                        )
-                        # Проверка кода ответа
-                        if response.status_code == 200:
-                            # Сохранение HTML-страницы целиком
-                            with open(file_name_html, "w", encoding="utf-8") as file:
-                                file.write(response.text)
-                            time.sleep(5)
+            # Проверка на существование файла out.json
+            if os.path.exists(json_file_path):
+                with open(json_file_path, "r", encoding="utf-8") as json_file:
+                    # Загрузка данных JSON
+                    data = json.load(json_file)
+                    # Получение всех ссылок из ключа "motodom_ua"
+                    links = data.get(directory, [])
+                    for l in links:
+                        url = l["link"]
+                        id_link = l["id"]
+                        file_name_html = os.path.join(html_path, f"{id_link}.html")
+                        get_html_motodom_ua(url, file_name_html)
 
 
+def get_html_motodom_ua(url, file_name_html):
+
+    if not os.path.exists(file_name_html):
+        cookies = {
+            "jrv": "55750",
+            "PHPSESSID": "5pfnuqtt1bh94bqaqs9qfv5ctu",
+            "default": "imrum01m616km4mgu38a4b0vpr",
+            "currency": "UAH",
+            "cf_clearance": "u7PFikBy.dJRMui6wRUUCmWA4Wbdu1nvD5FgD36KfvY-1723014419-1.0.1.1-QMhdwCrE.tZv_sGpWTNOXRP_x3OLH_jgX5.QlhvoSNGdK_JOqyi3V2oRHsp7_DbTqy11voEP.pI0u2DVX0V0Gg",
+            "language_url": "ru",
+            "language": "ru-ru",
+        }
+
+        headers = {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-language": "ru,en-US;q=0.9,en;q=0.8,uk;q=0.7,de;q=0.6",
+            "cache-control": "no-cache",
+            "dnt": "1",
+            "pragma": "no-cache",
+            "priority": "u=0, i",
+            "sec-ch-ua": '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Windows"',
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "none",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+        }
+        response = requests.get(
+            url,
+            cookies=cookies,
+            headers=headers,
+        )
+        # Проверка кода ответа
+        if response.status_code == 200:
+            # Сохранение HTML-страницы целиком
+            with open(file_name_html, "w", encoding="utf-8") as file:
+                file.write(response.text)
+            logger.info(f"Файл сохраненн {file_name_html}")
+            time.sleep(5)
+
+
+# Основная функция парсера
 async def parse_html_file():
     dir_path = os.path.join(os.getcwd(), "temp")
 
@@ -159,139 +168,67 @@ async def parse_html_file():
         d for d in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, d))
     ]
 
-    for directory in subdirs[2:3]:
-        subdir_path = os.path.join(dir_path, directory)
-        json_out = os.path.join(subdir_path, "out.json")
-        async with aiofiles.open(json_out, "r", encoding="utf-8") as file:
-            content = await file.read()
-            json_data = json.loads(content)
-
-        html_path = os.path.join(subdir_path, "html")
-        out_path = os.path.join(subdir_path, "out.json")
-        folder = os.path.join(html_path, "*.html")
-        files_html = glob.glob(folder)
-
-        for item in files_html:
-            id_product = os.path.splitext(os.path.basename(item))[0]
-            async with aiofiles.open(item, "r", encoding="utf-8") as file:
-                html_content = await file.read()
-
-            tree = HTMLParser(html_content)
-            # Цвета и размеры
-            # # Извлечение цветов и размеров из HTML
-            # color_map = {}
-            # size_map = {}
-            # color_id = None
-            # size_id = None
-
-            # # Извлечение цветов и размеров
-            # for option_div in tree.css(".form-group.product-option-radio"):
-            #     label_element = option_div.css_first(".control-label")
-            #     if not label_element:
-            #         continue
-
-            #     label = label_element.text().strip().lower()
-
-            #     option_id_element = option_div.css_first("div[id^='input-option']")
-            #     if not option_id_element:
-            #         continue
-
-            #     option_id = option_id_element.attributes.get("id").replace(
-            #         "input-option", ""
-            #     )
-
-            #     if "цвет" in label:
-            #         color_id = option_id
-            #         for radio in option_div.css(".radio"):
-            #             input_element = radio.css_first("input[type='radio']")
-            #             value = input_element.attributes.get("value")
-            #             option_value = radio.css_first(".option-value").text().strip()
-            #             color_map[value] = option_value
-            #         logger.debug(f"Color ID: {color_id}, Color Map: {color_map}")
-            #     elif "размер" in label:
-            #         size_id = option_id
-            #         for radio in option_div.css(".radio"):
-            #             input_element = radio.css_first("input[type='radio']")
-            #             value = input_element.attributes.get("value")
-            #             option_value = radio.css_first(".option-value").text().strip()
-            #             size_map[value] = option_value
-            #         logger.debug(f"Size ID: {size_id}, Size Map: {size_map}")
-
-            # # Обработка JSON-данных из скрипта
-            # for script_tag in tree.css("script[type='text/javascript']"):
-            #     script_content = script_tag.text()
-            #     if script_content:
-            #         json_match = re.search(
-            #             r"ro_params\['ro_data'\] = (.*?]);", script_content, re.DOTALL
-            #         )
-            #         if json_match:
-            #             json_string = json_match.group(1)
-            #             data = json.loads(json_string)
-
-            #             # Совместить идентификаторы с названиями цветов и размеров
-            #             combined_options = []
-            #             for entry in data:
-            #                 try:
-            #                     for ro_id, ro_info in entry["ro"].items():
-            #                         color_value = ro_info["options"].get(color_id)
-            #                         size_value = ro_info["options"].get(size_id)
-
-            #                         color_name = color_map.get(
-            #                             color_value, "Unknown Color"
-            #                         )
-            #                         size_name = size_map.get(size_value, "Unknown Size")
-
-            #                         combined_options.append(
-            #                             {
-            #                                 "related_option_id": ro_id,
-            #                                 "color": color_name,
-            #                                 "size": size_name,
-            #                             }
-            #                         )
-
-            #                         logger.debug(f"  Related Option ID: {ro_id}")
-            #                         logger.debug(f"    Color: {color_name}")
-            #                         logger.debug(f"    Size: {size_name}")
-            #                 except Exception as e:
-            #                     logger.error(f"Ошибка при обработке продукта: {e}")
-            #                     continue
-
-            available = None
-            price = None
-            stock_text_node = tree.css_first("#product > div.product-stats")
-
-            if stock_text_node:
-                stock_li_nodes = stock_text_node.css("li")
-                for li_node in stock_li_nodes:
-                    if "Наличие:" in li_node.text():
-                        stock_text_node = li_node.css_first("span")
-                        if stock_text_node:
-                            available = stock_text_node.text(strip=True)
-                # logger.info(available)
-
-            price_selector = "div.price-group > div.product-price"
-            price = extract_text(tree, price_selector)
-
-            if price == "Элемент не найден":
-                price_selector = "div.price-group > div.product-price-new"
-                price = extract_text(tree, price_selector)
-
-            err = None
-            data = {
-                "id": id_product,
-                "available": available,
-                "price": price,
-                "err": err,
-            }
-            logger.info(data)
-            update_json_with_price_and_availability(subdir_path, data)
+    for directory in subdirs:
+        if directory == "motodom_ua":
+            await parser_motodom_ua(dir_path, directory)
 
 
-def update_json_with_price_and_availability(subdir_path, data):
+# Парсер motodom_ua
+async def parser_motodom_ua(dir_path, directory):
+    subdir_path = os.path.join(dir_path, directory)
+    json_out = os.path.join(subdir_path, "out.json")
+
+    async with aiofiles.open(json_out, "r", encoding="utf-8") as file:
+        content = await file.read()
+        json_data = json.loads(content)
+
+    html_path = os.path.join(subdir_path, "html")
+    folder = os.path.join(html_path, "*.html")
+    files_html = glob.glob(folder)
+
+    for item in files_html:
+        id_product = os.path.splitext(os.path.basename(item))[0]
+        async with aiofiles.open(item, "r", encoding="utf-8") as file:
+            html_content = await file.read()
+
+        tree = HTMLParser(html_content)
+        available = None
+        price = None
+        stock_text_node = tree.css_first("#product > div.product-stats")
+
+        if stock_text_node:
+            stock_li_nodes = stock_text_node.css("li")
+            for li_node in stock_li_nodes:
+                if "Наличие:" in li_node.text():
+                    stock_text_node = li_node.css_first("span")
+                    if stock_text_node:
+                        available = stock_text_node.text(strip=True)
+
+        available = True if available == "В наличии" else False
+
+        price_selector = "div.price-group > div.product-price"
+        price = extract_price_motodom_ua(tree, price_selector)
+
+        if price == "Элемент не найден":
+            price_selector = "div.price-group > div.product-price-new"
+            price = extract_price_motodom_ua(tree, price_selector)
+
+        price = int(price)
+        err = None
+        data = {
+            "id": id_product,
+            "available": available,
+            "price": price,
+            "err": err,
+        }
+        logger.info(data)
+        result_motodom_ua(subdir_path, data)
+
+
+# Формирование резултата motodom_ua
+def result_motodom_ua(subdir_path, data):
     out_path = os.path.join(subdir_path, "out.json")
-    result_path = os.path.join(
-        subdir_path, "result.json"
-    )  # Путь для сохранения результата
+    result_path = os.path.join(subdir_path, "result.json")
 
     # Чтение данных из out.json
     if os.path.exists(out_path):
@@ -303,33 +240,30 @@ def update_json_with_price_and_availability(subdir_path, data):
         for key, items in json_data.items():
             for item in items:
                 if item["id"] == data["id"]:
+                    logger.info(f"Updating item with ID: {item['id']}")
                     item["available"] = data["available"]
                     item["price"] = data["price"]
                     item["err"] = data["err"]
                     updated = True
 
-        # Если данные обновлены, сохранение их в файл result.json
+        # Если данные обновлены, сохранение их в файл out.json
         if updated:
+            with open(out_path, "w", encoding="utf-8") as file:
+                json.dump(json_data, file, ensure_ascii=False, indent=4)
+            logger.info(f"Data updated in {out_path}")
+
+            # Также сохранить обновленные данные в файл result.json
             with open(result_path, "w", encoding="utf-8") as file:
                 json.dump(json_data, file, ensure_ascii=False, indent=4)
-            print(f"Updated data for ID {data['id']} in {result_path}")
+            logger.info(f"Data also saved in {result_path}")
         else:
-            print(f"ID {data['id']} not found in {out_path}")
+            logger.warning(f"ID {data['id']} not found in {out_path}")
     else:
-        print(f"File {out_path} does not exist.")
-
-
-def find_value_by_keyword_size_color(options, keywords):
-    for key, value in options.items():
-        # Проверяем каждое ключевое слово на соответствие ключу
-        for keyword in keywords:
-            if re.search(keyword, key, re.IGNORECASE):
-                return value
-    return None
+        logger.error(f"File {out_path} does not exist.")
 
 
 # Функция для извлечения цены
-def extract_text(tree, selector):
+def extract_price_motodom_ua(tree, selector):
     try:
         node = tree.css_first(selector)
         if node:
@@ -341,15 +275,7 @@ def extract_text(tree, selector):
         return f"Ошибка извлечения: {e}"
 
 
-def find_value_by_keyword(options, keywords):
-    for key, value in options.items():
-        # Преобразуем ключ в строку, чтобы быть уверенными, что мы работаем с текстом
-        if any(re.search(keyword, key) for keyword in keywords):
-            return value
-    return None
-
-
 if __name__ == "__main__":
     # get_dir_json()
-    # create_html_and_read_csv()
-    asyncio.run(parse_html_file())
+    create_html_and_read_csv()
+    # asyncio.run(parse_html_file())
