8698af426508cd32d3ac125fd8dd1d4144362da0
zinchyk.a <a.zinchyk83@gmail.com>
2024-12-04 09:53:01 +0200

    04122024

--- go_databid_com/main.py
+++ go_databid_com/main.py
@@ -1,7 +1,29 @@
 import json
+import time
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
 
 import requests
 from configuration.logger_setup import logger
+
+# Путь к папкам
+current_directory = Path.cwd()
+json_page_diretory = current_directory / "json_page"
+configuration_directory = current_directory / "configuration"
+
+json_page_diretory.mkdir(exist_ok=True, parents=True)
+configuration_directory.mkdir(parents=True, exist_ok=True)
+file_proxy = configuration_directory / "roman.txt"
+
+
+# Загружаем прокси
+def load_proxies():
+    with open(file_proxy, "r", encoding="utf-8") as file:
+        proxies = [line.strip() for line in file]
+    return proxies
+
+
+proxies = load_proxies()
 
 
 def get_json_details():
@@ -85,6 +107,125 @@ def get_json_projectInvolvement():
         json.dump(json_data, f, ensure_ascii=False, indent=4)  # Записываем в файл
 
 
+def get_json_companydetails():
+    cookies = {
+        "messagesUtk": "a24cb7b40f42455191b94f513aa4b2c6",
+        "ASP.NET_SessionId": "bal1jnkkkrw3c4oae45yxxs3",
+    }
+
+    headers = {
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "ru,en;q=0.9,uk;q=0.8",
+        "Connection": "keep-alive",
+        # 'Cookie': 'messagesUtk=a24cb7b40f42455191b94f513aa4b2c6; ASP.NET_SessionId=bal1jnkkkrw3c4oae45yxxs3',
+        "DNT": "1",
+        "Referer": "https://go.databid.com/NewDashboard/",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+        "sec-ch-ua": '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+    }
+
+    params = {
+        "UserId": "106640",
+        "CompanyID": "31496",
+    }
+
+    response = requests.get(
+        "https://go.databid.com/newdashboard/api/api/CompanyDetails/GetCompanyDetails",
+        params=params,
+        cookies=cookies,
+        headers=headers,
+        timeout=30,
+    )
+    companyid = params["CompanyID"]
+    json_data = response.json()
+    with open(f"{companyid}_projectInvolvement.json", "w", encoding="utf-8") as f:
+        json.dump(json_data, f, ensure_ascii=False, indent=4)  # Записываем в файл
+
+
+def get_page_json():
+
+    cookies = {
+        "messagesUtk": "a24cb7b40f42455191b94f513aa4b2c6",
+        "ASP.NET_SessionId": "bal1jnkkkrw3c4oae45yxxs3",
+    }
+
+    headers = {
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "ru,en;q=0.9,uk;q=0.8",
+        "Connection": "keep-alive",
+        "Content-Type": "application/json",
+        # 'Cookie': 'messagesUtk=a24cb7b40f42455191b94f513aa4b2c6; ASP.NET_SessionId=bal1jnkkkrw3c4oae45yxxs3',
+        "DNT": "1",
+        "Origin": "https://go.databid.com",
+        "Referer": "https://go.databid.com/NewDashboard/",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+        "sec-ch-ua": '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+    }
+
+    json_data = {
+        "DueDateFrom": None,
+        "DueDateTo": None,
+        "EnteredDateFrom": None,
+        "EnteredDateTo": None,
+        "IndustryTypeID": "",
+        "IndustrySubTypeID": "",
+        "rptDow1List": "",
+        "rptDow2List": "",
+        "rptDow3List": "",
+        "KeywordMatch": "primary",
+        "RegionID": "",
+        "CountyID": "",
+        "rptStageList": "",
+        "rptStageStatusList": "",
+        "rptContractingMethodsList": "",
+        "rptProjectClassList": "",
+        "rptConstructionTypeList": "",
+        "rptSectorList": "",
+        "NewProjects": 0,
+        "SavedSearchName": "",
+        "gProjectSavedSearchID": 0,
+        "SelectAllIndusty": False,
+    }
+    for page in range(1, 2418):
+        file_name = json_page_diretory / f"page_0{page}.json"
+        if file_name.exists():
+            continue
+        url = f"https://go.databid.com/newdashboard/api/api/AdvanceProjectSearch/addAdvanceProjectSearch?UserID=106640&ContactID=106726&RegionList=1,2,8,14,16,7&pageNo={page}&pageSize=100&SortOrder=0&SortDirection=1&SearchTxt=&ResultType=0&SavedSearchIDs=[object%20Object]"
+        response = requests.post(
+            url,
+            cookies=cookies,
+            headers=headers,
+            json=json_data,
+            timeout=10,
+        )
+        try:
+            if response.status_code == 200:
+                json_data = response.json()
+                with open(file_name, "w", encoding="utf-8") as f:
+                    json.dump(
+                        json_data, f, ensure_ascii=False, indent=4
+                    )  # Записываем в файл
+                logger.info(file_name)
+            else:
+                break
+        except requests.exceptions.ReadTimeout:
+            logger.error(f"Тайм-аут на странице {page}. Завершаем цикл.")
+            break
+        except Exception as e:
+            logger.error(f"Произошла ошибка на странице {page}: {e}")
+            break
+
+
 def parsing_json_details():
     with open("280403_Details.json", encoding="utf-8") as file:
         # Прочитать содержимое JSON файла
@@ -164,8 +305,13 @@ def parsing_json_project():
 
 
 if __name__ == "__main__":
+    # main()
+    while True:
+        get_page_json()
+        time.sleep(10)
     # get_json_details()
     # get_json_projectInvolvement()
+    # get_json_companydetails()
 
     # parsing_json_details()
-    parsing_json_project()
+    # parsing_json_project()
