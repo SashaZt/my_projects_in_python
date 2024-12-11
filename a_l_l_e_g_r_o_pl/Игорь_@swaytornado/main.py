82b4015bebc35d900450df48e30520d30d42c09c
zinchyk.a <a.zinchyk83@gmail.com>
2024-12-11 15:48:12 +0200

    11122024

--- a_l_l_e_g_r_o_pl/Игорь_@swaytornado/main.py
+++ a_l_l_e_g_r_o_pl/Игорь_@swaytornado/main.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from urllib.parse import urlencode, urlparse
 
 from configuration.logger_setup import logger
-from dotenv import load_dotenv
+from dotenv import dotenv_values, load_dotenv
 from downloader import Downloader
 from writer import Writer
 
@@ -34,17 +34,33 @@ def link_formation():
     return full_url
 
 
-def main_loop():
+def get_env():
     env_path = os.path.join(os.getcwd(), "configuration", ".env")
+
     if os.path.isfile(env_path):  # Проверяем, существует ли файл
+        # Удаляем ранее загруженные переменные
+        for key in dotenv_values(env_path).keys():
+            if key in os.environ:
+                del os.environ[key]
+
+        # Перезагружаем .env
         load_dotenv(env_path)
+
+        # Читаем переменные
         min_count = int(os.getenv("MIN_COUNT", "50"))
         api_key = os.getenv("API_KEY")
         max_workers = int(os.getenv("MAX_WORKERS", "20"))
         url_start = link_formation()
+
         logger.info("Файл .env загружен успешно.")
+        return min_count, api_key, max_workers, url_start
     else:
         logger.error(f"Файл {env_path} не найден!")
+        return None
+
+
+def make_directory(url_start):
+
     # Извлекаем путь из URL
     path = urlparse(url_start).path
 
@@ -78,7 +94,7 @@ def main_loop():
 
     json_page_directory = temp_directory / "json_page"
     json_scrapy = temp_directory / "json_scrapy"
-
+    logger.info(f"Создаю папку {directory_name}")
     data_directory.mkdir(parents=True, exist_ok=True)
     csv_directory.mkdir(parents=True, exist_ok=True)
     xlsx_directory.mkdir(parents=True, exist_ok=True)
@@ -90,42 +106,71 @@ def main_loop():
 
     configuration_directory.mkdir(parents=True, exist_ok=True)
 
-    csv_output_file = csv_directory / f"{formatted_date}_{directory_name}.csv"
-    json_result = data_directory / "result.json"
-    xlsx_result = xlsx_directory / f"{formatted_date}_{directory_name}.xlsx"
-
-    # Создаем объекты классов
-    downloader = Downloader(
-        min_count,
-        api_key,
+    return (
+        directory_name,
+        formatted_date,
+        csv_directory,
+        data_directory,
+        xlsx_directory,
         html_files_directory,
-        csv_output_file,
         json_products,
         json_scrapy,
-        url_start,
-        max_workers,
-        json_result,
-        xlsx_result,
         json_page_directory,
-    )
-    writer = Writer(csv_output_file, json_result, xlsx_result)
-    parser = Parser(
-        min_count,
-        html_files_directory,
-        csv_output_file,
-        max_workers,
-        json_products,
-        json_page_directory,
+        temp_directory,
     )
 
+
+def main_loop():
     # Основной цикл программы
     while True:
+        min_count, api_key, max_workers, url_start = get_env()  # Перезагрузка данных
+        (
+            directory_name,
+            formatted_date,
+            csv_directory,
+            data_directory,
+            xlsx_directory,
+            html_files_directory,
+            json_products,
+            json_scrapy,
+            json_page_directory,
+            temp_directory,
+        ) = make_directory(url_start)
+
+        csv_output_file = csv_directory / f"{formatted_date}_{directory_name}.csv"
+        json_result = data_directory / "result.json"
+        xlsx_result = xlsx_directory / f"{formatted_date}_{directory_name}.xlsx"
+
+        # Создаем объекты классов
+        downloader = Downloader(
+            min_count,
+            api_key,
+            html_files_directory,
+            csv_output_file,
+            json_products,
+            json_scrapy,
+            url_start,
+            max_workers,
+            json_result,
+            xlsx_result,
+            json_page_directory,
+        )
+        writer = Writer(csv_output_file, json_result, xlsx_result)
+        parser = Parser(
+            min_count,
+            html_files_directory,
+            csv_output_file,
+            max_workers,
+            json_products,
+            json_page_directory,
+        )
+
         print(
             "\nВыберите действие:\n"
             "1. Скачивание страниц пагинации\n"
             "2. Асинхронное скачивание товаров\n"
             "3. Сохранение результатов\n"
-            # "4. Очистить временные папки\n"
+            "4. Запустить все этапы сразу!!!\n"
             "0. Выход"
         )
         choice = input("Введите номер действия: ")
@@ -142,9 +187,57 @@ def main_loop():
             writer.save_json_to_excel()
 
             all_results = parser.parsing_json()
+            # min_count, api_key, max_workers, url_start = get_env()
+            # make_directory()
+            (
+                directory_name,
+                formatted_date,
+                csv_directory,
+                data_directory,
+                xlsx_directory,
+                html_files_directory,
+                json_products,
+                json_scrapy,
+                json_page_directory,
+                temp_directory,
+            ) = make_directory(url_start)
 
-        # elif choice == "4":
-        #     shutil.rmtree(temp_directory)
+        elif choice == "4":
+            # Запуск метода для получения всех страниц HTML
+            logger.info("Запуск хождения по пагинации")
+            downloader.get_all_page_html()
+
+            # Запуск асинхронного метода для обработки URL
+            logger.info("Запуск скачивания товара")
+            asyncio.run(downloader.main_url())
+
+            # Парсинг HTML и получение результатов
+            logger.info("Запуск парсинга html страниц")
+            all_results = parser.parsing_html()
+
+            # Сохранение результатов в JSON и Excel
+            logger.info("Сохранение результатов в Excel")
+            writer.save_results_to_json(all_results)
+            writer.save_json_to_excel()
+
+            # Парсинг данных из JSON
+            logger.info("Сохранение результатов в JSON")
+            all_results = parser.parsing_json()
+            (
+                directory_name,
+                formatted_date,
+                csv_directory,
+                data_directory,
+                xlsx_directory,
+                html_files_directory,
+                json_products,
+                json_scrapy,
+                json_page_directory,
+                temp_directory,
+            ) = make_directory(url_start)
+        elif choice == "5":
+            shutil.rmtree(temp_directory)
+
         elif choice == "0":
             break
         else:
