6de53debeba5c19ad89152129372e67ad67b924b
zinchyk.a <a.zinchyk83@gmail.com>
2025-01-10 11:04:32 +0200

    10012025

--- ringostat_zubr/server/app/get_routes.py
+++ ringostat_zubr/server/app/get_routes.py
@@ -1,7 +1,9 @@
 import base64
 import os
 from datetime import datetime
+from typing import Optional
 
+from configuration.logger_setup import logger  # Импортируем loguru логгер
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
@@ -9,8 +11,6 @@ from dependencies import get_db
 from dotenv import load_dotenv
 from fastapi import APIRouter, Depends, HTTPException, Query
 from fastapi.responses import JSONResponse
-from typing import Optional
-from configuration.logger_setup import logger  # Импортируем loguru логгер
 
 # Создание экземпляра маршрутизатора
 router = APIRouter()
@@ -48,6 +48,7 @@ def decrypt_access_key(encrypted_key: str) -> str:
         raise
 
 
+# Первый вариант
 # @router.get("/get_all_data")
 # async def get_all_data(access_key: str = Query(...), db=Depends(get_db)):
 #     try:
@@ -256,39 +257,39 @@ def decrypt_access_key(encrypted_key: str) -> str:
 #         )
 
 
-# @router.get("/get_all_data")
-# async def get_all_data(access_key: str = Query(...), db=Depends(get_db)):
-#     try:
-#         decrypted_key = decrypt_access_key(access_key)
-#         if decrypted_key != ORIGINAL_ACCESS_KEY:
-#             logger.warning("Access denied: Invalid access key")
-#             raise HTTPException(
-#                 status_code=403, detail="Access denied: Invalid access key"
-#             )
+@router.get("/get_all_data")
+async def get_all_data(access_key: str = Query(...), db=Depends(get_db)):
+    try:
+        # decrypted_key = decrypt_access_key(access_key)
+        # if decrypted_key != ORIGINAL_ACCESS_KEY:
+        #     logger.warning("Access denied: Invalid access key")
+        #     raise HTTPException(
+        #         status_code=403, detail="Access denied: Invalid access key"
+        #     )
 
-#         # Простой запрос без фильтрации
-#         query = "SELECT * FROM calls_zubr"
+        # Простой запрос без фильтрации
+        query = "SELECT * FROM calls_zubr"
 
-#         logger.info("Executing SQL query to fetch all records")
-#         async with db.pool.acquire() as connection:
-#             async with connection.cursor() as cursor:
-#                 await cursor.execute(query)
-#                 records = await cursor.fetchall()
-#                 logger.info(f"Fetched {len(records)} records from database")
+        logger.info("Executing SQL query to fetch all records")
+        async with db.pool.acquire() as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute(query)
+                records = await cursor.fetchall()
+                logger.info(f"Fetched {len(records)} records from database")
 
-#         # Преобразование datetime в строку
-#         for record in records:
-#             for key, value in record.items():
-#                 if isinstance(value, datetime):
-#                     record[key] = value.strftime("%Y-%m-%d %H:%M:%S")
+        # Преобразование datetime в строку
+        for record in records:
+            for key, value in record.items():
+                if isinstance(value, datetime):
+                    record[key] = value.strftime("%Y-%m-%d %H:%M:%S")
 
-#         logger.info("Returning all records to client")
-#         return JSONResponse(
-#             status_code=200, content={"status": "success", "data": records}
-#         )
+        logger.info("Returning all records to client")
+        return JSONResponse(
+            status_code=200, content={"status": "success", "data": records}
+        )
 
-#     except Exception as e:
-#         logger.error(f"An error occurred while processing the request: {e}")
-#         return JSONResponse(
-#             status_code=500, content={"status": "failure", "message": str(e)}
-#         )
+    except Exception as e:
+        logger.error(f"An error occurred while processing the request: {e}")
+        return JSONResponse(
+            status_code=500, content={"status": "failure", "message": str(e)}
+        )
