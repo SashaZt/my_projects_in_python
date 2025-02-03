3bac4c5056bac0b125fb89bdb20e74640cd8ad49
zinchyk.a <a.zinchyk83@gmail.com>
2025-02-03 17:45:43 +0200

    03022025

--- ringostat_zubr/server/app/configuration/logger_setup.py
+++ ringostat_zubr/server/app/configuration/logger_setup.py
@@ -9,7 +9,8 @@ os.makedirs(logging_path, exist_ok=True)
 log_file_path = os.path.join(logging_path, "log_message.log")
 logger.add(
     log_file_path,
-    format="{time:DD-MM-YYYY HH:mm:ss} - {level} - {message}",
+    format="{time:DD-MM-YYYY HH:mm:ss} | {level} | {line} | {message}",
     level="DEBUG",
     encoding="utf-8",
+    colorize=True
 )
